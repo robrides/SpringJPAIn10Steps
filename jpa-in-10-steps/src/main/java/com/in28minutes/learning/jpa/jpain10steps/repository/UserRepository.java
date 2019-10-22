91ef9f70216cdec7ea8758b3a542aa82c822d59e
Rob Lounsbury <rob.lounsbury@gmail.com>
2019-10-22 16:29:22 -0500

    JPA Intro

---
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.in28minutes.learning.jpa.jpain10steps.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.in28minutes.learning.jpa.jpain10steps.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
